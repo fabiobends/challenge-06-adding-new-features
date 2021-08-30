28f845fcb56bdfe734d4b8929256ee361fd57fec
Fábio <fabio.mendes@profusion.com>
2021-08-29 21:20:09 -0300

    add: header tests

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,14 @@
-export default function Header() {
-  // TODO
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Header(): JSX.Element {
+  return (
+    <nav>
+      <Link href="/">
+        <a>
+          <Image src="/assets/logo.svg" alt="logo" width="240" height="26" />
+        </a>
+      </Link>
+    </nav>
+  );
 }
