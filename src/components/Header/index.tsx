73b204a92b37682146d7620c8153df58288d8339
Fábio <fabio.mendes@profusion.com>
2021-09-06 09:17:40 -0300

    add: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import commonStyles from '../../styles/common.module.scss';
+
+import styles from './header.module.scss';
+
 export default function Header(): JSX.Element {
   return (
-    <nav>
+    <nav className={`${commonStyles.container} ${styles.container}`}>
       <Link href="/">
         <a>
           <Image src="/assets/logo.svg" alt="logo" width="240" height="26" />
